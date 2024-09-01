c8c1933562367037b926da9d25ea0e172140ebf4
Anchal Kumar Sharma <anchalkumarsharma@Anchals-MacBook-Air.local>
2024-09-01 23:47:51 +0530

    Fixxesss

--- src/pages/main.js
+++ src/pages/main.js
@@ -6,12 +6,25 @@ import Exhibitions from "./exhibitions";
 import Explore from "./explore";
 import Contact from "./contact";
 import Visit from "./visit";
+import Guided from '../components/GuidedTours';
+import InteractiveExhibits from "../components/InteractiveExhibits";
+import EducationalWorkshops from "../components/EducationalWorkshops";
 
 const Main = () => {
   return (
     <div>
       <div id="/">
         <Hero />
+          
+        <div id="exhibitions" className="scroll-m-16">
+          <EducationalWorkshops />
+        </div>
+        <div id="exhibitions" className="scroll-m-16">
+          <Guided />
+        </div>
+        <div id="exhibitions" className="scroll-m-16">
+          <InteractiveExhibits />
+        </div>
         <div id="exhibitions" className="scroll-m-16">
           <Exhibitions />
         </div>
